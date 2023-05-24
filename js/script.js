af89d843d54914108367734621cc09685e8d8f77
charlotte-jhu <124389290+charlotte-jhu@users.noreply.github.com>
2023-05-24 22:12:47 +0000

    added webapge

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Charlotte Jhu
+// Created on: March 2023
 // This file contains the JS functions for index.html
+
+function myButtonClicked() {
+  // this is a cookie clicker game
+  // process
+if ( localStorage.clicks ) {
+  localStorage.clicks = Number(localStorage.clicks) + 1
+} else {
+  localStorage.clicks = 1
+}
+
+// output
+document.getElementById('clicks').innerHTML = "You have " + localStorage.clicks + " cookies!"
+}
